4e2e0361c59db3aec4a92db3189d3ca91e661ba8
darkkevin15 <Manpathuva@gmail.com>
2024-08-16 17:55:37 +0530

    Update confing.js

--- confing.js
+++ confing.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,|| ''
+SESSION_ID: process.env.SESSION_ID,|| '',
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.fm/u/gr7egn4vtj",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am Kevin-Md i am alive now..!",
 };
