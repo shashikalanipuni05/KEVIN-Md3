1d8a3c4ae25d70182b5a5f62d13c27a6fafb5f74
darkkevin15 <Manpathuva@gmail.com>
2024-08-18 13:52:37 +0530

    Update confing.js

--- confing.js
+++ confing.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,|| 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.fm/u/2zuqxy3aps",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am Kevin-Md i am alive now..!",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true" false,
 };
