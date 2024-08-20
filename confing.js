0d10b7df958582867e377c9ce6f0ff28fdc08c9c
darkkevin15 <Manpathuva@gmail.com>
2024-08-20 10:23:15 +0530

    Update confing.js

--- confing.js
+++ confing.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,||
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3c64b5608dd82d33dabe8.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am Kevin-Md i am alive now..!",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true" false,
+MODE: process.env.MODE || "public",
 };
