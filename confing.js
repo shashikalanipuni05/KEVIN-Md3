a1a5a9eb6a8fa6e953ec5b83ce881134a9951c29
darkkevin15 <Manpathuva@gmail.com>
2024-08-24 12:48:28 +0530

    Update confing.js

--- confing.js
+++ confing.js
@@ -6,8 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,|| 
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3c64b5608dd82d33dabe8.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am Kevin-Md i am alive now..!",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true" false,
-MODE: process.env.MODE || "public", // *public or inbox or groups*
+MONGODB: process.env.MONGODB || "enter mlngo here",
 };
