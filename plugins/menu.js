51067bba879855a38af2d8335e49c68b7b8211a9
darkkevin15 <Manpathuva@gmail.com>
2024-08-24 13:07:13 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,4 +1,4 @@
-const config = require('../config')
+const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 
 cmd({
@@ -9,6 +9,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+const config = await readEnv();
 let menu = {
 main: '',
 download: '',
@@ -20,7 +21,7 @@ search: '',
 
   for (let i = 0; i < commands.length; i++) {
 if (commands[i].pattern && !commands[i].dontAddCommandList) {
-menu[commands[i].category] += `.${commands[i].pattern}\n`;
+menu[commands[i].category] += `${config.PREFIX}${commands[i].pattern}\n`;
  }
 }
 
@@ -51,9 +52,7 @@ ${menu.search}
 
 POWERD BY KEVIN_MD 🩵
 `
-  await conn.sendMassage(from,{image:{url"
-https://telegra.ph/file/3c64b5608dd82d33dabe8.jpg
-"},caption:madeMenu},{quoted:mek})
+  await conn.sendMassage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
     
 }catch(e){
 console.log(e)
