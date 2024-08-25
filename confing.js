3b8c4ae7bf39d86d74e899818274c7a88831b6f3
darkkevin15 <Manpathuva@gmail.com>
2024-08-25 05:55:36 +0530

    Update confing.js

--- confing.js
+++ confing.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,|| 
-MONGODB: process.env.MONGODB || "enter mlngo here",
+SESSION_ID: process.env.SESSION_ID || "",
+MONGODB: process.env.MONGODB || "enter mango url here"
 };
