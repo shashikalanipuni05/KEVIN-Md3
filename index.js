d4f7631bb5b45d54fbe149b4cf494afaf6f05d9f
Ayodya Vichakshana <o.g.ayodyavichakshana@gmail.com>
2024-08-27 14:55:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ mek = mek.messages[0]
 if (!mek.message) return	
 mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 //<=========Auto status view=============
-if (config.STATUS_VIEW === 'true') {
+if (config.AUTO_READ_STATUS === 'true') {
 if (mek.key && mek.key.remoteJid === 'status@broadcast') {
 await conn.readMessages([mek.key])
 }
