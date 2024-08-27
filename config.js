01849f8aa02a3728ae348c40fd74e9d7308c281b
Ayodya Vichakshana <o.g.ayodyavichakshana@gmail.com>
2024-08-27 14:49:02 +0530

    Update and rename confing.js to config.js

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "HF1UQADa#XZS9YQGF7_ZG0Qm2ltEEopjKExAN-k1SNOniYaT1-os",
+STATUS_VIEW: process.env.STATUS_VIEW === undefined ? 'true' : process.env.STATUS_VIEW,    
 MONGODB: process.env.MONGODB || "mongodb://mongo:YwaWQTJTuUHeFHPSoTZvLunISgkvYvsE@autorack.proxy.rlwy.net:12762"
 };
